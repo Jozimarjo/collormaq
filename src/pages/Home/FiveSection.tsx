0b020956c9d34e46e3a73db3953398dd3f1d7bad
Jozimar <jozimar@motorola.com>
2022-01-20 18:00:16 -0400

    five-scetion mobile adjustmenst

--- src/pages/Home/FiveSection.tsx
+++ src/pages/Home/FiveSection.tsx
@@ -1,11 +1,12 @@
 import { Box, Button, Divider, Flex, HStack, Icon, Input, Text, VStack } from "@chakra-ui/react";
 import { RiFacebookFill, RiInstagramLine, RiLinkedinFill, RiWhatsappLine } from "react-icons/ri";
+import { ScreenSize } from "../../interfaces/ScreenSize";
 
-export default function FiveSection() {
+export default function FiveSection({ isWideVersion = true }: ScreenSize) {
     return (
-        <Box w='100%' height='900px' backgroundImage="linear-gradient(69deg, rgba(10,27,74,1) 0%, rgba(48,95,60,1) 79%)">
-            <Box p='82px' display='flex' justifyContent='center' align='center' width='100%'>
-                <Flex width='82%' justify="space-evenly">
+        <Box w='100%' backgroundImage="linear-gradient(69deg, rgba(10,27,74,1) 0%, rgba(48,95,60,1) 79%)">
+            <Box p={{ base: '30px', md: '82px' }} display='flex' justifyContent='center' align='center' width='100%'>
+                <Flex width={{ base: '100%', lg: '82%' }} justify={{ base: 'space-between', lg: 'space-evenly' }}>
                     <Icon fontSize='30' as={RiFacebookFill} />
                     <Icon fontSize='30' as={RiInstagramLine} />
                     <Icon fontSize='30' as={RiLinkedinFill} />
@@ -13,52 +14,99 @@ export default function FiveSection() {
                 </Flex>
             </Box>
 
-            <Flex justify="center" pl='146px'>
+            <Flex justify="center" pl={{ base: '0', lg: '146px' }}>
                 <Box width="82%">
-                    <HStack spacing="16">
+                    <HStack className="MEUTESTE" alignItems={{ base: 'start', lg: 'center' }} flexDirection={{ base: 'column', lg: 'row' }} spacing={{ base: '0', lg: '16' }}>
                         <Flex direction="column">
-                            <Text fontSize="48px" fontWeight='bold'>Collormaq</Text>
-                            <Text fontSize="16px" fontWeight='normal' maxWidth="480px">
+                            <Text mb='30px' fontSize="48px" fontWeight='bold'>Collormaq</Text>
+                            <Text mb='40px' fontSize="16px" fontWeight='normal' maxWidth="480px">
                                 O sistema de impressão em flexografia e offset
                                 combinado com a alta qualificação da equipe garantem
                                 o atendimento de forma personalizada a cada cliente.
                             </Text>
-                            <Flex>
+                            <Flex direction={{ base: 'column', lg: 'row' }}>
                                 <Input mr='4' variant='flushed' placeholder='Email' />
-                                <Button width='6.25em' bg='transparent' border='1px solid #FFFFF'>
+                                <Button width={{ base: '100%', lg: '6.25em' }} bg='transparent' my='4' border='1px solid #FFFFFF'>
                                     <Text fontWeight='bold' >Enviar</Text>
                                 </Button>
                             </Flex>
                         </Flex>
-                        <Divider orientation='vertical' height='214px'></Divider>
+                        {isWideVersion && (<Divider orientation='vertical' height='214px'></Divider>)}
 
-                        <VStack spacing='4'>
-                            <Text as='h1' fontSize='21' width='100px'>
-                                Menu
-                            </Text>
 
-                            <Text width='100px' fontSize='16'>
-                                Informação
-                            </Text>
-                            <Text width='100px' fontSize='16'>
-                                Informação
-                            </Text>
-                            <Text width='100px' fontSize='16'>
-                                Informação
-                            </Text>
-                            <Text width='100px' fontSize='16'>
-                                Informação
-                            </Text>
-                            <Text width='100px' fontSize='16'>
-                                Informação
-                            </Text>
-                        </VStack>
+                        <Flex flexWrap='wrap' justify={{ base: 'sapce-between', lg: 'space-around' }}>
+                            <VStack mb='20px' mr='20px' alignItems={{ base: 'start', lg: 'center' }} spacing='4'>
+                                <Text as='h1' fontSize='21' width='100px'>
+                                    Menu
+                                </Text>
+
+                                <Text width='100px' fontSize='16'>
+                                    Informação
+                                </Text>
+                                <Text width='100px' fontSize='16'>
+                                    Informação
+                                </Text>
+                                <Text width='100px' fontSize='16'>
+                                    Informação
+                                </Text>
+                                <Text width='100px' fontSize='16'>
+                                    Informação
+                                </Text>
+                                <Text width='100px' fontSize='16'>
+                                    Informação
+                                </Text>
+                            </VStack>
+                            <VStack mb='20px' mr='20px'alignItems={{ base: 'start', lg: 'center' }} spacing='4'>
+                                <Text as='h1' fontSize='21' width='100px'>
+                                    Menu
+                                </Text>
+
+                                <Text width='100px' fontSize='16'>
+                                    Informação
+                                </Text>
+                                <Text width='100px' fontSize='16'>
+                                    Informação
+                                </Text>
+                                <Text width='100px' fontSize='16'>
+                                    Informação
+                                </Text>
+                                <Text width='100px' fontSize='16'>
+                                    Informação
+                                </Text>
+                                <Text width='100px' fontSize='16'>
+                                    Informação
+                                </Text>
+                            </VStack>
+                            <VStack mb='20px' mr='20px'alignItems={{ base: 'start', lg: 'center' }} spacing='4'>
+                                <Text as='h1' fontSize='21' width='100px'>
+                                    Menu
+                                </Text>
+
+                                <Text width='100px' fontSize='16'>
+                                    Informação
+                                </Text>
+                                <Text width='100px' fontSize='16'>
+                                    Informação
+                                </Text>
+                                <Text width='100px' fontSize='16'>
+                                    Informação
+                                </Text>
+                                <Text width='100px' fontSize='16'>
+                                    Informação
+                                </Text>
+                                <Text width='100px' fontSize='16'>
+                                    Informação
+                                </Text>
+                            </VStack>
+                        </Flex>
+
                     </HStack>
+
 
                 </Box>
             </Flex>
 
-            <Divider></Divider>
+            {isWideVersion && (<Divider mt='15em' ></Divider>)}
 
         </Box >
     )
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
                 <SecondSection />
                 <ThirdSection />
                 <FourthSection />
-                <FiveSection />
+                <FiveSection isWideVersion={isWideVersion} />
             </Flex>
         </>
     )
