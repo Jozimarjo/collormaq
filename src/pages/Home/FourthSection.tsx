3def72db2874f5742db5e21e93fe20e3b440e43e
Jozimar <jozimar@motorola.com>
2022-01-19 21:44:06 -0400

    mobile screen fourth section

--- src/pages/Home/FourthSection.tsx
+++ src/pages/Home/FourthSection.tsx
@@ -2,9 +2,9 @@ import { Box, Flex, Image, SimpleGrid, Text } from "@chakra-ui/react";
 export default function FourthSection() {
     return (
         <Box>
-            <Flex align="center" justifyContent="center">
-                <Flex direction="column" justify="center" p='50px'>
-                    <Text fontWeight="bold" maxWidth='min-content'  mb='7' fontSize="48px" color="#262626" lineHeight="64px">
+            <Flex align="center" justifyContent="center" direction={{ base: 'column', md: 'row' }}>
+                <Flex direction="column" justify="center" p={{ base: '30px', lg: '50px' }}>
+                    <Text fontWeight="bold" maxWidth={{ base: '100%', lg: 'min-content' }} mb='7' fontSize={{ base: "28px", md: "48px" }} color="#262626" lineHeight={{ base: "34px", lg: "64px" }}>
                         Clientes Parceiros
                     </Text>
                     <Text fontSize="16px" maxWidth="480px" color="#262626">
@@ -12,8 +12,8 @@ export default function FourthSection() {
                         produzindo produtos de alta qualidade para o mercado de flexografia.
                     </Text>
                 </Flex>
-                <Box>
-                    <SimpleGrid columns={2} spacing={10} gridColumnGap='170px' gridGap={0} gridTemplateColumns='repeat(3, minmax(0, 118px))'>
+                <Box px={{ base: '30px', lg: '50px' }} py={{ base: '30px', lg: '50px' }} >
+                    <SimpleGrid columns={2} spacing={10} gridColumnGap={{ base: '0', xl: '170px' }} gridGap={0} gridTemplateColumns={{ base: 'repeat(3, minmax(0, 1fr))', lg: 'repeat(3, minmax(0, 118px))' }}>
                         <Box>
                             <Image src='/images/parceiro.svg' />
                         </Box>
