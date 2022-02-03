4b3e3802b317865e4c4cdb0209a19a655c8ed8b4
Jozimar <jozimar@motorola.com>
2022-02-03 15:36:31 -0400

    contato page

--- src/pages/contato/index.tsx
+++ src/pages/contato/index.tsx
@@ -1,9 +1,12 @@
-import { Box, Button, Flex, Image, Stack, Text, useBreakpointValue, VStack } from "@chakra-ui/react";
+import { Box, Button, Flex, Icon, Image, Input, Text, Textarea, useBreakpointValue, VStack } from "@chakra-ui/react";
 import { Carousel } from "react-bootstrap";
+import { RiMailOpenFill, RiPhoneFill } from "react-icons/ri";
 import { EntreContato } from "../../components/EntreContato";
 
 import FooterComponent from "../../components/Footer";
 import Header from "../../components/Header";
+import CityCard from "./CityCard";
+import OurStores from "./OurStores";
 
 export default function Contato() {
     const isWideVersion = useBreakpointValue({
@@ -12,6 +15,8 @@ export default function Contato() {
         md: false,
         lg: true
     });
+
+
     return (
         <>
             <Box position='relative'>
@@ -23,109 +28,49 @@ export default function Contato() {
                         </Box>
                     </Carousel.Item>
                 </Carousel>
+                <Flex p='30px' justify="space-evenly" my='100px' flexWrap='wrap' >
+                    <VStack spacing='30px' align="start" p='30px' w='540px' height='780' background='white' direction='column' mb={{ base: '50px', xl: '0' }} >
+                        <Text color='#305F3C' fontSize='48' fontWeight='bold'>Fale Conosco</Text>
+                        <Input focusBorderColor='#305F3C' variant='flushed' placeholder='Nome' />
+                        <Input focusBorderColor='#305F3C' variant='flushed' placeholder='E-mail' />
+                        <Input focusBorderColor='#305F3C' variant='flushed' placeholder='Numero' />
+                        <Input focusBorderColor='#305F3C' variant='flushed' placeholder='Escolha a Unidade Collormar' />
+                        <Textarea focusBorderColor='#305F3C' variant='flushed' placeholder='Digite aqui sua mensagem...'></Textarea>
+                        <Button width='12.5em' bg='#305F3C' variant='solid'>
+                            <Text fontWeight='bold' >Enviar</Text>
+                        </Button>
+                    </VStack>
+                    <VStack spacing='30px' align="start" direction='column' >
 
-                <Box width='100%' p='30px' background='#F5F6F7' borderBottom='1px solid #eee' textAlign='center'>
-                    <Flex direction='column' align='center' mb='70px'>
-                        <Text textAlign={{ base: 'left', md: 'center' }} mb='8' fontSize={{ base: '28', md: '48' }} color='#305F3C' fontWeight='bold'>Visite uma de nossas lojas</Text>
-                        <Text textAlign={{ base: 'left', md: 'center' }} maxWidth='586px' fontSize='16' color='#262626'>Dispomos de 03 Unidades para melhor servi-lo com qualidade e equipe especializada a tirar todas as suas dúvidas.</Text>
-                    </Flex>
+                        <Flex px='30px' direction='column' justify="center" align="center" maxWidth='540px' height='240px' background='white'>
+                            <Text color='#305F3C' fontSize='48' fontWeight='bold'>Manaus</Text>
+                            <Box alignSelf={{ base: 'start', md: 'center' }}>
+                                <Text color='#305F3C' fontSize={{ base: '18', md: '24' }} fontWeight='bold'><Icon color='#305F3C' w={8} h={8} as={RiPhoneFill} />(092) 3654-7373 | 3634-6204</Text>
+                                <Text color='#305F3C' fontSize={{ base: '18', md: '24' }} fontWeight='bold'><Icon color='#305F3C' w={8} h={8} as={RiMailOpenFill} /> collormar@gmail.com </Text>
+                            </Box>
+                            <Text color='#707070' fontSize='16' fontWeight='bold'>Funcionamos de Seg à Sex de 07:30h às 12h, 13:15 às 17:15h</Text>
+                        </Flex>
+                        <Flex px='30px' direction='column' justify="center" align="center" maxWidth='540px' height='240px' background='white'>
+                            <Text color='#305F3C' fontSize='48' fontWeight='bold'>Boa Vista</Text>
+                            <Box alignSelf={{ base: 'start', md: 'center' }}>
+                                <Text color='#305F3C' fontSize={{ base: '18', md: '24' }} fontWeight='bold'><Icon color='#305F3C' w={8} h={8} as={RiPhoneFill} />(95) 3626-0550 | 3626-0838</Text>
+                                <Text color='#305F3C' fontSize={{ base: '18', md: '24' }} fontWeight='bold'><Icon color='#305F3C' w={8} h={8} as={RiMailOpenFill} /> collormar@gmail.com </Text>
+                            </Box>
+                            <Text color='#707070' fontSize='16' fontWeight='bold'>Funcionamos de Seg à Sex de 07:30h às 12h, 13:15 às 17:15h</Text>
+                        </Flex>
+                        <Flex px='30px' direction='column' justify="center" align="center" maxWidth='540px' height='240px' background='white'>
+                            <Text color='#305F3C' fontSize='48' fontWeight='bold'>Porto Velho</Text>
+                            <Box alignSelf={{ base: 'start', md: 'center' }}>
+                                <Text color='#305F3C' fontSize={{ base: '18', md: '24' }} fontWeight='bold'><Icon color='#305F3C' w={8} h={8} as={RiPhoneFill} />(69) 3224-8204 | 3026-3103</Text>
+                                <Text color='#305F3C' fontSize={{ base: '18', md: '24' }} fontWeight='bold'><Icon color='#305F3C' w={8} h={8} as={RiMailOpenFill} /> collormar@gmail.com </Text>
+                            </Box>
+                            <Text color='#707070' fontSize='16' fontWeight='bold'>Funcionamos de Seg à Sex de 07:30h às 12h, 13:15 às 17:15h</Text>
+                        </Flex>
 
-                    <Stack direction={{ base: 'column', md: 'row' }} justify='center' align='center' spacing="8">
+                    </VStack>
+                </Flex>
 
-                        <Box maxW='sm' overflow='hidden'>
-                            <Image src='/images/empresa_front.svg' alt='img alt' />
-
-                            <VStack align='start' spacing='4' direction='column'>
-                                <Box display='flex' alignItems='baseline'>
-                                    <Text
-                                        color='#305F3C'
-                                        fontWeight='bold'
-                                        letterSpacing='wide'
-                                        fontSize='36'
-                                    >
-                                        Manaus
-                                    </Text>
-                                </Box>
-
-                                <Text
-                                    color='#262626'
-                                    fontSize={{ base: '13px', md: '13px', lg: '16' }}
-                                    textAlign='left'
-                                >
-                                    Rua Maurice Ravel, 175 -
-                                    Shangrila VII - Parque Dez de Novembro. CEP: 69054-759 - Manaus/AM.
-                                    Fones: (92) 3654-7373 / 3634-6204. E-mail: collormaq.am@collormaq.com.br
-                                </Text>
-
-                                <Button width='168px' bg='#305F3C' variant='solid'>
-                                    <Text fontWeight='bold' >Ver no mapa</Text>
-                                </Button>
-                            </VStack>
-                        </Box>
-
-                        <Box maxW='sm' overflow='hidden'>
-                            <Image src='/images/empresa_front.svg' alt='img alt' />
-
-                            <VStack align='start' spacing='4' direction='column'>
-                                <Box display='flex' alignItems='baseline'>
-                                    <Text
-                                        color='#305F3C'
-                                        fontWeight='bold'
-                                        letterSpacing='wide'
-                                        fontSize='36'
-                                    >
-                                        Porto Velho
-                                    </Text>
-                                </Box>
-
-                                <Text
-                                    color='#262626'
-                                    fontSize={{ base: '13px', md: '13px', lg: '16' }}
-                                    textAlign='left'
-                                >
-                                    Av. Guaporé, 395 - Agenor de Carvalho. CEP: 76.820-277 - Porto Velho/RO.
-                                    Fones: (69) 3224-8204 / 3026-3103.
-                                    E-mail: collormaq.ro@collormaq.com.br
-                                </Text>
-
-                                <Button width='168px' bg='#305F3C' variant='solid'>
-                                    <Text fontWeight='bold' >Ver no mapa</Text>
-                                </Button>
-                            </VStack>
-                        </Box>
-
-                        <Box maxW='sm' overflow='hidden'>
-                            <Image src='/images/empresa_front.svg' alt='img alt' />
-
-                            <VStack align='start' spacing='4' direction='column'>
-                                <Box display='flex' alignItems='baseline'>
-                                    <Text
-                                        color='#305F3C'
-                                        fontWeight='bold'
-                                        letterSpacing='wide'
-                                        fontSize='36'
-                                    >
-                                        Boa Vista
-                                    </Text>
-                                </Box>
-
-                                <Text
-                                    color='#262626'
-                                    fontSize={{ base: '13px', md: '13px', lg: '16' }}
-                                    textAlign='left'
-                                >
-                                    Av. Ataide Teive, 3270 - Buritis. CEP: 69000-000 - Boa Vista/RR.
-                                    Fones: (95) 3626-0550 / 3626-0838. 
-                                    E-mail: collormaq.rr@collormaq.com.br
-                                </Text>
-
-                                <Button width='168px' bg='#305F3C' variant='solid'>
-                                    <Text fontWeight='bold' >Ver no mapa</Text>
-                                </Button>
-                            </VStack>
-                        </Box>
-                    </Stack>
-                </Box>
+                <OurStores />
                 <EntreContato />
                 <FooterComponent isWideVersion={isWideVersion} />
             </Box>
