7666ff36ac2f350a1ef2f4796a80ea89844c520f
Jozimar <jozimar@motorola.com>
2022-02-22 15:09:58 -0400

    SEO adjustments contato

--- src/pages/contato/OurStores.tsx
+++ src/pages/contato/OurStores.tsx
@@ -5,10 +5,10 @@ export default function OurStores() {
     const cards = [
         {
             img: '/images/quem_somos_section_1.svg', title: 'Manaus',
-            description: 'Rua Maurice Ravel, 175 - Shangrila VII - Parque Dez de Novembro.CEP: 69054 - 759 - Manaus / AM. Fones: (92) 3654 - 7373 / 3634 - 6204. E - mail: collormaq.am@collormaq.com.br'
+            description: 'Rua Maurice Ravel, 175 - Shangrila VII - Parque Dez de Novembro.CEP: 69054 - 759 - Manaus / AM.'
         },
-        { img: '/images/quem_somos_section_1.svg', title: 'Boa Vista', description: 'Av. Ataide Teive, 3270 - Buritis. CEP: 69000-000 - Boa Vista/RR. Fones: (95) 3626-0550 / 3626-0838. E-mail: collormaq.rr@collormaq.com.br' },
-        { img: '/images/quem_somos_section_1.svg', title: 'Porto Velho', description: 'Av. Guaporé, 395 - Agenor de Carvalho. CEP: 76.820-277 - Porto Velho/RO. Fones: (69) 3224-8204 / 3026-3103. E-mail: collormaq.ro@collormaq.com.br' },
+        { img: '/images/quem_somos_section_1.svg', title: 'Boa Vista', description: 'Av. Ataide Teive, 3270 - Buritis. CEP: 69000-000 - Boa Vista/RR.' },
+        { img: '/images/quem_somos_section_1.svg', title: 'Porto Velho', description: 'Av. Guaporé, 395 - Agenor de Carvalho. CEP: 76.820-277 - Porto Velho/RO.'},
     ]
 
     return (
