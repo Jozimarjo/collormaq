776197e39164d01d2e8cefee4e0b1014be4b93ba
Jozimar <jozimar@motorola.com>
2022-03-02 16:52:40 -0400

    adjust home page url

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 const withImages = require('next-images')
 const withPlugins = require('next-compose-plugins');
 
-// module.exports = {
-//   reactStrictMode: true,
-//   disableStaticImages: true,
-//   withImages: withImages(),
-//   redirects: async () => {
-//     return [
-//       {
-//         source: '/',
-//         destination: '/Home', // Matched parameters can be used in the destination
-//         permanent: true,
-//       },
-//     ]
-//   },
-
-// }
 module.exports = withPlugins([
   [withImages], {
     reactStrictMode: true,
