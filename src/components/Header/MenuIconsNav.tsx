e3b067137c944cca53c572b24987a4de632f4b4f
Jozimar <jozimar@motorola.com>
2022-03-03 16:04:08 -0400

    adjust home

--- src/components/Header/MenuIconsNav.tsx
+++ src/components/Header/MenuIconsNav.tsx
@@ -4,13 +4,13 @@ import { RiSearchLine } from 'react-icons/ri'
 export function MenuIconsNav() {
     return (
         <HStack as='nav' spacing="4" mx="8" pr="8" py="1" color="gray.300"  borderColor="gray.700" >
-            <Link>
+            <Link href="/">
                 <Text fontWeight='bold' color='white'>Home</Text>
             </Link>
-            <Link>
+            <Link href="/quem_somos">
                 <Text fontWeight='bold' color='white'>Quem somos</Text>
             </Link>
-            <Link>
+            <Link href="/contato">
                 <Text fontWeight='bold' color='white'>Contato</Text>
             </Link>
             <Link>
